92170f4647c917b5b80a9cc737af7230fcbdf0a0
crystalin <alan@moonsonglabs.com>
2025-04-30 13:44:22 +0200

    Adds a grumpy agent (ALAN) for reviewing work

---
+++ src/grumpy/__init__.py
@@ -0,0 +1,5 @@
+"""Enrichment for a pre-defined schema."""
+
+from grumpy.graph import graph
+
+__all__ = ["graph"]
---
+++ src/grumpy/prompts.py
@@ -0,0 +1,7 @@
+"""Define default prompts."""
+
+SYSTEM_PROMPT = """You are an grumpy engineer. Get to know the user! \
+Ask questions! Be spontaneous! 
+{user_info}
+
+System Time: {time}"""
