d3bf827e9157fbc3c9264ecf406b218cec0a67f6
Nisheeth Barthwal <nbaztec@gmail.com>
2025-05-06 14:42:08 +0200

    cleanup

--- tests/testing/evaluators.py
+++ tests/testing/evaluators.py
@@ -11,20 +11,27 @@ class LLMJudge:
         *,
         model: str = "google_genai:gemini-2.0-flash-lite",
     ):
+        """Create a LLM as a judge using preset defaults.
+
+        Args:
+            model: Uses Gemini-2.0-flash-lite.
+
+        Returns:
+            A LLMJudge object that can create evaluators.
+        """
         self.model = model
 
     def create_correctness_evaluator(
         self, continuous: bool = True, prompt: str = CORRECTNESS_PROMPT, **kwargs
     ) -> Union[SimpleEvaluator, Callable[..., Any]]:
-        """Create a LLM as a judge evaluator using preset defaults.
+        """Create a correctness evaluator using preset defaults.
 
         Args:
             continuous: True.
             prompt: Uses CORRECTNESS_PROMPT.
-            model: Uses Gemini-2.0-flash-lite.
 
         Returns:
-            A function with the
+            An evaluator function that takes inputs, outputs and reference_outputs.
         """
 
         def correctness_evaluator(inputs: dict, outputs: dict, reference_outputs: dict):
@@ -34,7 +41,7 @@ class LLMJudge:
                 model=self.model,
                 **kwargs,
             )
-            
+
             eval_result = evaluator(
                 inputs=inputs,
                 outputs=outputs,
@@ -43,41 +50,6 @@ class LLMJudge:
             return eval_result
 
         return correctness_evaluator
-
-
-# def correctness_evaluator(inputs: dict, outputs: dict, reference_outputs: dict):
-#     evaluator = create_correctness_judge()
-#     outputs_contents = outputs["output"]
-#     reference_outputs_contents = reference_outputs["message"]["content"]
-
-#     eval_result = evaluator(
-#         inputs=inputs,
-#         outputs=outputs_contents,
-#         reference_outputs=reference_outputs_contents,
-#     )
-#     return eval_result
-
-
-# def create_correctness_judge(
-#     *,
-#     continuous: bool = True,
-#     prompt: str = CORRECTNESS_PROMPT,
-#     model: str = "google_genai:gemini-2.0-flash-lite",
-#     **kwargs,
-# ):
-#     """Create a LLM as a judge using preset defaults.
-
-#     Args:
-#         continuous: True.
-#         prompt: Uses CORRECTNESS_PROMPT.
-#         model: Uses Gemini-2.0-flash-lite.
-#     """
-#     return create_llm_as_judge(
-#         prompt=prompt,
-#         continuous=continuous,
-#         model=model,
-#         **kwargs,
-#     )
 
 
 __all__ = [
