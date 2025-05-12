e5f54810c8509c56edc8bfa8624177e1d36439a3
elfedy <federico@moonsonglabs.com>
2025-05-12 17:15:25 -0300

    add code review flow

--- src/coder/lg_server.py
+++ src/coder/lg_server.py
@@ -1,6 +1,6 @@
 """Coder graph for LangGraph Server."""
 
-from coder.graph import graph_builder
+from coder.graph import coder_change_request_config, coder_new_pr_config
 from coder.mocks import MockGithubApi
 from coder.tools import get_github_tools
 
@@ -8,7 +8,10 @@ from coder.tools import get_github_tools
 # TODO: maybe we can setup this to use mock/real github api depending on env/config
 mock_api = MockGithubApi()
 github_tools = get_github_tools(mock_api)
-graph = graph_builder(github_tools).compile()
-graph.name = "Coder"
 
-__all__ = ["graph"]
+graph_new_pr = coder_new_pr_config().graph_builder(github_tools).compile()
+graph_change_request = (
+    coder_change_request_config().graph_builder(github_tools).compile()
+)
+
+__all__ = ["graph_new_pr", "graph_change_request"]
