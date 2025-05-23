a68f8ae73e0a48c5fee66879d00b1704e2f274e0
elfedy <federico@moonsonglabs.com>
2025-05-23 17:37:43 -0300

    I believe in you coderabbit

--- src/common/logging.py
+++ src/common/logging.py
@@ -6,12 +6,15 @@ import os
 
 def logger(name: str):
     """Initialize the logger."""
-    logger = logging.getLogger(name)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(os.getenv("LOG_LEVEL", "INFO").upper())
-    return logger
+    log = logging.getLogger(name)
+
+    if not log.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+
+    log.setLevel(os.getenv("LOG_LEVEL", "INFO").upper())
+    return log
