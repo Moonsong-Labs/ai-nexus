6d51e7a30c03a31084d6f92ad6448dcb293d960b
Stephen Shelton <steve@brewcraft.org>
2025-06-09 09:56:17 -0600

    WIP: test code reviewe tool invocation

---
+++ tests/unit_tests/test_code_reviewer.py
@@ -0,0 +1,55 @@
+import pytest
+import tempfile
+import uuid
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.store.memory import InMemoryStore
+from testing.inputs import decode_base_messages
+
+from code_reviewer.graph import CodeReviewerGraph, local_code_reviewer_config
+from code_reviewer.state import State
+
+@pytest.mark.asyncio
+async def test_tool_call_none() -> None:
+    code_reviewer = CodeReviewerGraph(
+        checkpointer=InMemorySaver(),
+        store=InMemoryStore(),
+        github_tools=[],
+        config=local_code_reviewer_config()
+    )
+
+    result = await code_reviewer.compiled_graph.ainvoke(
+        State(
+            messages=[HumanMessage(content="Hi")],
+        ),
+        code_reviewer.create_runnable_config({"configurable": {"thread_id": str(uuid.uuid4())}}),
+    )
+
+    last_message = result["messages"][-1]
+    actual_tool_calls = [t["name"] for t in getattr(last_message, "tool_calls", [])]
+
+    assert [] == actual_tool_calls
+    
+@pytest.mark.asyncio
+async def test_reads_dir() -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        code_reviewer = CodeReviewerGraph(
+            checkpointer=InMemorySaver(),
+            store=InMemoryStore(),
+            github_tools=[],
+            config=local_code_reviewer_config()
+        )
+
+        prompt = f"Please review the code in the project found in project dir {temp_dir}"
+
+        result = await code_reviewer.compiled_graph.ainvoke(
+            State(
+                messages=[HumanMessage(content=prompt)],
+            ),
+            code_reviewer.create_runnable_config({"configurable": {"thread_id": str(uuid.uuid4())}}),
+        )
+
+        last_message = result["messages"][-1]
+        actual_tool_calls = [t["name"] for t in getattr(last_message, "tool_calls", [])]
+
+        assert ["TODO: should have read dir call here"] == actual_tool_calls
